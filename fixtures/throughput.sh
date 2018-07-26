db8f0156140f5cd59559f3adb1172ca07937b4f7
Benjamin Bengfort <benjamin@bengfort.com>
2018-07-26 11:51:52 -0400

    benchmark up to 42 clients

--- fixtures/throughput.sh
+++ fixtures/throughput.sh
@@ -6,14 +6,14 @@ RESULTS="throughput-0.3-local.csv"
 
 RUNS=12
 MIN_CLIENTS=1
-MAX_CLIENTS=12
+MAX_CLIENTS=42
 
 # Describe the time format
 TIMEFORMAT="experiment completed in %2lR"
 
 time {
   # Write header to the output file
-  # echo "server,clients,messages,duration,throughput" >> $RESULTS
+  echo "server,clients,messages,duration,throughput" >> $RESULTS
 
 
   for (( I=0; I<=$RUNS; I+=1 )); do
@@ -26,19 +26,14 @@ time {
           UPTIME=10s
         fi
 
-        # for SERVER in simple sequence actor locker ; do
-        #
-        #   ipseity serve -u $UPTIME -s $SERVER &
-        #   sleep 1
-        #   ipseity bench -s $SERVER -c $J >> $RESULTS &
-        #   wait
-        #
-        # done
-
-        ipseity serve -u $UPTIME -s stream &
-        sleep 1
-        ipseity bench -s stream -c $J >> $RESULTS &
-        wait
+        for SERVER in stream simple sequence actor locker ; do
+       
+          ipseity serve -u $UPTIME -s $SERVER &
+          sleep 1
+          ipseity bench -s $SERVER -c $J >> $RESULTS &
+          wait
+       
+        done
 
       done
   done
